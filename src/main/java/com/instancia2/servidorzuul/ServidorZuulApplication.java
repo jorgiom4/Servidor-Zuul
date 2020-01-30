b332dad8c7c7f3e906ecc036479158a34ab64f7e
Gorka López-Quintana <>
2020-01-30 11:59:42 +0100

    Primer commit del proyecto

---
+++ src/main/java/com/instancia2/servidorzuul/ServidorZuulApplication.java
@@ -0,0 +1,14 @@
+package com.instancia2.servidorzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorZuulApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServidorZuulApplication.class, args);
+	}
+
+}
